5e9e36d1f5efae80fe9ea461c40de9a95facccc9
Thiago <thiagomowszet@gmail.com>
2024-04-14 21:46:07 -0300

    session 7

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,47 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"net/http"
-	"strconv"
 	"errors"
+	"fmt"
+	// "html/template"
+	"net/http"
 	"github.com/thiagomowszet/snippetbox/internal/models"
+	"strconv"
 )
 
-// Change the signature of the home handler so it is defined as a method against
-// *application.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
-	files := []string{
-		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
-	}
-
-	ts, err := template.ParseFiles(files...)
-
+	snippets, err := app.snippets.Latest()
 	if err != nil {
-		// Because the home handler function is now a method against application
-		// it can access its fields, including the error logger. We'll write the log
-		// message to this instead of the standard logger.
-		app.errorLog.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 		return
 	}
 
-	err = ts.ExecuteTemplate(w, "base", nil)
-
-	if err != nil {
-		// Also update the code here to use the error logger from the application
-		// struct.
-		app.errorLog.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+
+	// files := []string{
+	// "./ui/html/base.tmpl",
+	// "./ui/html/partials/nav.tmpl",
+	// "./ui/html/pages/home.tmpl",
+	// }
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// app.serverError(w, err)
+	// return
+	// }
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// app.serverError(w, err)
+	// }
 }
 
 // Change the signature of the snippetView handler so it is defined as a method
--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -49,12 +49,9 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	stmt := `SELECT id, title, content, created, expires FROM snippets
 	WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-
 	row := m.DB.QueryRow(stmt, id)
 
-
 	s := &Snippet{}
-
 
 	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 
@@ -67,4 +64,56 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 	}
 	// If everything went OK then return the Snippet object.
 	return s, nil
+}
+
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	// Write the SQL statement we want to execute.
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+			 WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+	// Use the Query() method on the connection pool to execute our
+	// SQL statement. This returns a sql.Rows resultset containing the result of
+	// our query.
+	rows, err := m.DB.Query(stmt)
+
+	if err != nil {
+		return nil, err
+	}
+	// We defer rows.Close() to ensure the sql.Rows resultset is
+	// always properly closed before the Latest() method returns. This defer
+	// statement should come *after* you check for an error from the Query()
+	// method. Otherwise, if Query() returns an error, you'll get a panic
+	// trying to close a nil resultset.
+	defer rows.Close()
+	// Initialize an empty slice to hold the Snippet structs.
+	snippets := []*Snippet{}
+	// Use rows.Next to iterate through the rows in the resultset. This
+	// prepares the first (and then each subsequent) row to be acted on by the
+	// rows.Scan() method. If iteration over all the rows completes then the
+	// resultset automatically closes itself and frees-up the underlying
+	// database connection.
+	for rows.Next() {
+		// Create a pointer to a new zeroed Snippet struct.
+		s := &Snippet{}
+		// Use rows.Scan() to copy the values from each field in the row to the
+		// new Snippet object that we created. Again, the arguments to row.Scan()
+		// must be pointers to the place you want to copy the data into, and the
+		// number of arguments must be exactly the same as the number of
+		// columns returned by your statement.
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+		if err != nil {
+			return nil, err
+		}
+		// Append it to the slice of snippets.
+		snippets = append(snippets, s)
+	}
+	// When the rows.Next() loop has finished we call rows.Err() to retrieve any
+	// error that was encountered during the iteration. It's important to
+	// call this - don't assume that a successful iteration was completed
+	// over the whole resultset.
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	// If everything went OK then return the Snippets slice.
+	return snippets, nil
 }
