c8d35368f9ca5f738d37a737b9be923f68cb7ab6
Thiago <thiagomowszet@gmail.com>
2024-04-22 22:41:13 -0300

    session 8

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,9 +3,9 @@ package main
 import (
 	"errors"
 	"fmt"
-	// "html/template"
-	"net/http"
 	"github.com/thiagomowszet/snippetbox/internal/models"
+	"html/template"
+	"net/http"
 	"strconv"
 )
 
@@ -45,14 +45,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 // against *application.
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
 	}
 
-	// Use the SnippetModel object's Get method to retrieve the data for a
-	// specific record based on its ID. If no matching record is found,
-	// return a 404 Not Found response.
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
@@ -62,8 +60,30 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	// Write the snippet data as a plain-text HTTP response body.
-	fmt.Fprintf(w, "%+v", snippet)
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/view.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	// Create an instance of a templateData struct holding the snippet data.
+	data := &templateData{
+		Snippet: snippet,
+	}
+
+	// Pass in the templateData struct when executing the template.
+	err = ts.ExecuteTemplate(w, "base", data)
+
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
 
 // Change the signature of the snippetCreate handler so it is defined as a method
