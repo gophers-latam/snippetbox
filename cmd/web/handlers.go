0efa3fb38d3667c9c9821a73c340ec1bc4b18dcf
Juan David Merchan Torres <juan1014166@gmail.com>
2024-08-11 16:38:01 -0500

    feat(web-handlers): Add default values to snippetCreateForm

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -62,6 +62,14 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 
+	// Initialize a new createSnippetForm instance and pass it to the template.
+	// Notice how this is also a great opportunity to set any default or
+	// 'initial' values for the form --- here we set the initial value for the
+	// snippet expiry to 365 days.
+	data.Form = snippetCreateForm{
+		Expires: 7,
+	}
+
 	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
